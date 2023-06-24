9e126204732f52aaecbc01fd5d89ad9f3b72ef1b
Charlton Trezevant <ct@ctis.me>
2023-06-23 21:14:05 -0400

    analytics: add separate frontend js tracking for prod/non-prod

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -1,6 +1,22 @@
 module AnalyticsHelper
   def analytics_tag
-    site_id = Rails.configuration.fathom
-    javascript_include_tag "https://cdn.usefathom.com/script.js", defer: true, data: {site: site_id, auto: false}
+    if Rails.env.production?
+      javascript_tag do
+        <<-JS.html_safe
+        !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){if(window.analytics.initialized)return window.analytics[e].apply(window.analytics,arguments);var i=Array.prototype.slice.call(arguments);i.unshift(e);analytics.push(i);return analytics}};for(var i=0;i<analytics.methods.length;i++){var key=analytics.methods[i];analytics[key]=analytics.factory(key)}analytics.load=function(key,i){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=i};analytics._writeKey="J9KwHw1TSixl07WeR3ZkoHlHVLKDBfdf";;analytics.SNIPPET_VERSION="4.16.1";
+        analytics.load("J9KwHw1TSixl07WeR3ZkoHlHVLKDBfdf");
+        analytics.page();
+        }}();
+        JS
+      end
+    else
+      javascript_tag do
+        <<-JS.html_safe
+        !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){if(window.analytics.initialized)return window.analytics[e].apply(window.analytics,arguments);var i=Array.prototype.slice.call(arguments);i.unshift(e);analytics.push(i);return analytics}};for(var i=0;i<analytics.methods.length;i++){var key=analytics.methods[i];analytics[key]=analytics.factory(key)}analytics.load=function(key,i){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=i};analytics._writeKey="PO60GSoD811FoEE2PD9FoMUHMcX5NXt5";;analytics.SNIPPET_VERSION="4.16.1";
+        analytics.load("PO60GSoD811FoEE2PD9FoMUHMcX5NXt5");
+        analytics.page();
+        }}();
+        JS
+      end    end
   end
 end
