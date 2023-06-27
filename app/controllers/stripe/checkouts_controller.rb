125776ab55bfa85f4e2d712682c1f93ed1e1c79a
Charlton Trezevant <ct@ctis.me>
2023-06-27 12:19:04 -0400

    checkouts_controller: require a business profile to exist before allowing someone to check out

--- app/controllers/stripe/checkouts_controller.rb
+++ app/controllers/stripe/checkouts_controller.rb
@@ -2,6 +2,7 @@ module Stripe
   class CheckoutsController < ApplicationController
     before_action :authenticate_user!
     before_action :require_signed_hiring_agreement!
+    before_action :require_business_profile!
 
     def create
       redirect_to BusinessSubscriptionCheckout.new(
@@ -12,6 +13,12 @@ module Stripe
     end
 
     private
+
+    def require_business_profile!
+      if current_user.business.nil? or !current_user.business.present?
+        redirect_to new_business_path, notice: I18n.t("errors.business_subscription_no_profile")
+      end
+    end
 
     def require_signed_hiring_agreement!
       if current_user.needs_to_sign_hiring_agreement?
