0c6f942f010829a58a03e05debe7e2261eda7cb9
Charlton Trezevant <ct@ctis.me>
2023-12-13 10:41:42 -0500

    Added nicole's testimonial

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Testimonial = Struct.new(:image, :name, :testimonial, :title, :url, keyword_init: true) do
   def self.all
     # TODO: New testimonials
-    # Rails.configuration.testimonials.map { |h| new(h) }
+    Rails.configuration.testimonials.map { |h| new(h) }
   end
 end
